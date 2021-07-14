5a699c94fb04efa2874492e608d065f8cfe9892b
Nitin Reddy <nitin.reddy@appdirect.com>
2021-07-14 15:27:44 +0530

    Configured Travis ci

--- src/appdirect-root-config.ts
+++ src/appdirect-root-config.ts
@@ -1,19 +1,17 @@
 import { registerApplication, start } from "single-spa";
 
-registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
-  activeWhen: ["/"],
-});
 
-// registerApplication({
-//   name: "@appdirect/navbar",
-//   app: () => System.import("@appdirect/navbar"),
-//   activeWhen: ["/"]
-// });
+registerApplication({
+  name: "@appdirect/react-app",
+  app: () => System.import( "@appdirect/react-app" ),
+  activeWhen: [ "/react" ]
+} );
+
+registerApplication( {
+  name: "angular-app",
+  app: () => System.import( "angular-app" ),
+  activeWhen: [ "/angular" ]
+} );
 
 start({
   urlRerouteOnly: true,
